19f053468ec7e428166e0d30d8c92d6a9e93bcbe
sachin kumar saw <sachinsaw@globussoft.in>
2021-05-31 17:27:40 +0530

    Initial Commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by GLB-311-PC on 31/05/21.
+//  Copyright © 2021 GLB-311-PC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
